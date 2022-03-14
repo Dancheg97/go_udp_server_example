fc44c651edc1a66bfa6d20b5b000eac555980e5e
Danila Fomin <dangdancheg@gmail.com>
2022-03-14 16:39:21 +0300

    .

--- dockerfile
+++ dockerfile
@@ -2,4 +2,5 @@ FROM golang:latest
 RUN mkdir /app 
 ADD . /app/ 
 WORKDIR /app 
+EXPOSE 4271:4271
 RUN go run .
--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"time"
+	"log"
+	"net"
 )
 
 func main() {
-	v, e := os.LookupEnv("EXPIRATION")
-	fmt.Println(e, v)
-	time.Sleep(time.Second)
+	pc, err := net.ListenPacket("udp", ":4271")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pc.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		n, addr, err := pc.ReadFrom(buf)
+		if err != nil {
+			continue
+		}
+		go serve(pc, addr, buf[:n])
+	}
+}
+
+func serve(pc net.PacketConn, addr net.Addr, buf []byte) {
+	buf[2] |= 0x80
+	pc.WriteTo(buf, addr)
+
+	fmt.Println(buf)
 }
